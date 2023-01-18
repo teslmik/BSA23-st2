2441dfde03db023cc679a0e484f4726b7b053eb6
Teslmik <teslmik@gmail.com>
2023-01-18 11:54:55 +0200

    implement fight function

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -6,76 +6,80 @@ export async function fight(firstFighter, secondFighter) {
     // resolve the promise with the winner when fight is over
     const rightFighterIndicator = document.getElementById('right-fighter-indicator'),
       leftFighterIndicator = document.getElementById('left-fighter-indicator'),
+      CRIT_FREQUENCY = 10000,
       canAction = {
-        isBlockOne: false,
-        isBlockTwo: false,
-        canCriticalOne: true,
-        canCriticalTwo: true,
+        isBlockLeft: false,
+        isBlockRight: false,
+        canCriticalLeft: true,
+        canCriticalRight: true,
       };
 
-    let healthFirst = 100,
-      healthSecond = 100,
-      criticalKeyOne = new Map(),
-      criticalKeyTwo = new Map();
+    let healthLeft = 100,
+      healthRight = 100,
+      criticalKeyLeft = new Map(),
+      criticalKeyRight = new Map();
+
+    const keyupFunc = (event) => {
+      const key = event.code;
+      if (key === controls.PlayerOneBlock) canAction.isBlockLeft = false;
+      if (key === controls.PlayerTwoBlock) canAction.isBlockRight = false;
+      if (controls.PlayerOneCriticalHitCombination.includes(key)) criticalKeyLeft.set(key, false);
+      if (controls.PlayerTwoCriticalHitCombination.includes(key)) criticalKeyRight.set(key, false);
+    }
 
     // -------------------------------------------------------------------
 
     document.addEventListener('keydown', playerActions);
-    document.addEventListener('keyup', (e) => {
-      const key = e.code;
-      if (key === controls.PlayerOneBlock) canAction.isBlockOne = false;
-      if (key === controls.PlayerTwoBlock) canAction.isBlockTwo = false;
-      if (controls.PlayerOneCriticalHitCombination.includes(key)) criticalKeyOne.set(key, false);
-      if (controls.PlayerTwoCriticalHitCombination.includes(key)) criticalKeyTwo.set(key, false);
-    });
-
-    if (healthSecond === 0) resolve(firstFighter);
-    if (healthFirst === 0) resolve(secondFighter);
+    document.addEventListener('keyup', keyupFunc);
 
     // ---------------------------------------------------------
 
     function playerActions(event) {
       const key = event.code;
 
-      if (key === controls.PlayerOneAttack && !canAction.isBlockOne && !canAction.isBlockTwo) {
-        healthSecond = attackFighter(firstFighter, secondFighter, healthSecond);
-        rightFighterIndicator.style.width = `${healthSecond}%`;
+      if (key === controls.PlayerOneAttack && !canAction.isBlockLeft && !canAction.isBlockRight) {
+        healthRight = attackFighter(firstFighter, secondFighter, healthRight);
+        rightFighterIndicator.style.width = `${healthRight}%`;
       }
-      if (key === controls.PlayerTwoAttack && !canAction.isBlockTwo && !canAction.isBlockOne) {
-        healthFirst = attackFighter(secondFighter, firstFighter, healthFirst);
-        leftFighterIndicator.style.width = `${healthFirst}%`;
+      if (key === controls.PlayerTwoAttack && !canAction.isBlockRight && !canAction.isBlockLeft) {
+        healthLeft = attackFighter(secondFighter, firstFighter, healthLeft);
+        leftFighterIndicator.style.width = `${healthLeft}%`;
       }
-      if (key === controls.PlayerOneBlock) canAction.isBlockOne = true;
-      if (key === controls.PlayerTwoBlock) canAction.isBlockTwo = true;
+      if (key === controls.PlayerOneBlock) canAction.isBlockLeft = true;
+      if (key === controls.PlayerTwoBlock) canAction.isBlockRight = true;
 
       if (controls.PlayerOneCriticalHitCombination.includes(key)) {
-        criticalKeyOne.set(key, true);
+        criticalKeyLeft.set(key, true);
 
-        if (criticalKeyOne.get('KeyQ') && criticalKeyOne.get('KeyW') && criticalKeyOne.get('KeyE') && canAction.canCriticalOne) {
-
-          canAction.canCriticalOne = false;
-          healthSecond = attackFighter(firstFighter, secondFighter, healthSecond, true);
-          rightFighterIndicator.style.width = `${healthSecond}%`;
+        if (criticalKeyLeft.get('KeyQ') && criticalKeyLeft.get('KeyW') && criticalKeyLeft.get('KeyE') && canAction.canCriticalLeft) {
+          canAction.canCriticalLeft = false;
+          healthRight = attackFighter(firstFighter, secondFighter, healthRight, true);
+          rightFighterIndicator.style.width = `${healthRight}%`;
 
           setTimeout(() => {
-            canAction.canCriticalOne = true;
-          }, 10000);
+            canAction.canCriticalLeft = true;
+          }, CRIT_FREQUENCY);
         }
       }
 
       if (controls.PlayerTwoCriticalHitCombination.includes(key)) {
-        criticalKeyTwo.set(key, true);
+        criticalKeyRight.set(key, true);
 
-        if (criticalKeyTwo.get('KeyU') && criticalKeyTwo.get('KeyI') && criticalKeyTwo.get('KeyO') && canAction.canCriticalTwo) {
-
-          canAction.canCriticalTwo = false;
-          healthFirst = attackFighter(secondFighter, firstFighter, healthFirst, true);
-          leftFighterIndicator.style.width = `${healthFirst}%`;
+        if (criticalKeyRight.get('KeyU') && criticalKeyRight.get('KeyI') && criticalKeyRight.get('KeyO') && canAction.canCriticalRight) {
+          canAction.canCriticalRight = false;
+          healthLeft = attackFighter(secondFighter, firstFighter, healthLeft, true);
+          leftFighterIndicator.style.width = `${healthLeft}%`;
 
           setTimeout(() => {
-            canAction.canCriticalTwo = true;
-          }, 10000);
+            canAction.canCriticalRight = true;
+          }, CRIT_FREQUENCY);
         }
+      }
+
+      if (healthRight === 0 || healthLeft === 0) {
+        document.removeEventListener('keydown', playerActions);
+        document.removeEventListener('keyup', keyupFunc);
+        healthRight === 0 ? resolve(firstFighter) : resolve(secondFighter);
       }
     }
   });
@@ -92,7 +96,6 @@ function attackFighter(attacking, attacked, healthCount, critical = false) {
 export function getDamage(attacker, defender) {
   // return damage
   const damage = getHitPower(attacker) - getBlockPower(defender);
-
   return damage > 0 ? damage : 0;
 }
 
@@ -103,6 +106,5 @@ export function getHitPower(fighter) {
 
 export function getBlockPower(fighter) {
   // return block power
-
   return fighter.defense * (Math.random() + 1);
 }
