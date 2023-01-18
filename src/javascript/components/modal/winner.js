98e76801464b22f78c49f2bfb6c4eda0bb851b6c
Teslmik <teslmik@gmail.com>
2023-01-18 11:55:44 +0200

    create showModal function

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,18 @@
+import { showModal } from './modal';
+import { createFighterImage } from '../fighterPreview';
+import { createElement } from '../../helpers/domHelper';
+
 export function showWinnerModal(fighter) {
   // call showModal function 
+  const winnerPreview = createElement({
+    tagName: 'div',
+    className: 'winner-preview',
+  });
+
+  let imageElement = createFighterImage(fighter, "left");
+  let title = `${fighter.name} WON!! `;
+
+  winnerPreview.append(imageElement);
+
+  showModal({ title, bodyElement: winnerPreview, onClose: () => window.location.reload() });
 }
